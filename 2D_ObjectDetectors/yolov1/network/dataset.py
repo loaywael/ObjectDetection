2c120957c5fbef353a87f2f2e305440d10566444
loaywael <hythamconn1@gmail.com>
2020-12-21 01:42:59 +0200

    implemented dataset class, iou_util

---
+++ 2D_ObjectDetectors/yolov1/network/dataset.py
@@ -0,0 +1,56 @@
+import os
+import glob
+import torch
+import numpy as np
+import pandas as pd
+from PIL import Image
+from network import DATA_PATH
+
+
+
+class VOCDataset(torch.utils.data.Dataset):
+    def __init__(self, csv_file, img_dir, label_dir, S=7, B=2, C=20, tansform=False):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.label_dir = label_dir
+        self.tansform = tansform
+        self.S = S
+        self.B = B
+        self.C = C
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def _norm_box_dims(self, box):
+        x, y, w, h = box
+        cx, cy = self.S*x - j, self.S*y - i     # relative to cell origin
+        w, h = self.S*w, self.S*h               # equivalent cell steps
+        return [cx, cy, w, h]
+
+    def _denorm_box_dim(self, box):
+        pass
+
+    def __getitem__(self, index):
+        label_path = os.path.join(self.label_dir, self.annotations.iloc[index, 1])
+        annotations = []
+        with open(label_path) as f:
+            for label in f.readlines():
+                annotations.append([eval(val) for val in label.strip().split()])
+        img_path = os.path.join(self.img_dir, self.annotations.iloc[index, 0])
+        img = Image.open("img_path")
+        annotations = torch.tensor(annotations)
+        if self.tansform:
+            img, boxes = self.tansform(img, annotations)
+        # target output shape --> (S, S, C+5)
+        target_matrix = torch.zeros((self.S, self.S, self.C + 5*self.B))
+        # PIL image shape --> (w, h, c)
+        for class_id, box in annotations:
+            # identify the box cell
+            i, j = int(y * self.S), int(x * self.S)
+            # normalize dims relative to cell dims
+            normed_box = self._norm_box_dims(box)
+            target_matrix[i, j, [self.C, class_id] = 1
+            target_matrix[i, j, self.C+1:] = torch.tensor(normed_box)
+            
+        return img, target_matrix   
+# VOCDataset()
--- 2D_ObjectDetectors/yolov1/network/utils.py
+++ 2D_ObjectDetectors/yolov1/network/utils.py
@@ -3,5 +3,40 @@ import  numpy as np
 
 
 
-def intersection_over_union(ground_truth, predicted_anchor):
-    pass
+def intersection_over_union(target_boxes, predicted_boxes):
+    """
+    Evaluates Predicted box location relative to Ground Truth box
+    based on Intersection over Union metric to score high matching.
+
+    Params
+    ------
+    target_boxes : (torch.tensor)
+        midpoint format boxes {cx, cy, w, h} of shape --> (N, 4) 
+    predicted_boxes : (torch.tensor)
+        {cx, cy, w, h} of shape --> (N, 4) 
+    
+    Return
+    ------
+    iou_scores : (torch.tensor)
+        iou scores of shape --> (N, 1)
+    """
+    tb_cx, tb_cy, tb_w, tb_h  = target_boxes.split(1, dim=1)
+    pb_cx, pb_cy, pb_w, pb_h = predicted_boxes.split(1, dim=1)
+    # midpoint anchor box format
+    tb_x1, tb_y1 = tb_cx - tb_w/2, tb_cy - tb_h/2
+    tb_x2, tb_y2 = tb_cx + tb_w/2, tb_cy + tb_h/2
+    pb_x1, pb_y1 = pb_cx - pb_w, pb_cy - pb_h
+    pb_x2, pb_y2 = pb_cx + pb_w/2, pb_cy + pb_h/2
+    # intersection corners estimation
+    inter_x1 = torch.max(tb_x1, bp_x1)
+    inter_y1 = torch.max(tb_y1, bp_y1)
+    inter_x2 = torch.min(tb_x2, bp_x2)
+    inter_y2 = torch.min(tb_y2, bp_y2)
+    # iou evaluation
+    inter_area = (inter_x2-inter_x1).clamp(0) * (inter_y2-inter_y1).clamp(0)
+    union_area = torch.abs((tb_w * tb_h) + (pb_w * pb_h)) - inter_area
+    iou_scores = inter_area / union_area
+    
+    return iou_scores + 1e-6
+    
+    
